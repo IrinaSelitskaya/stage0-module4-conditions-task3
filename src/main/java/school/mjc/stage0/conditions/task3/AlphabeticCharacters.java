29672887beb804165abc29c8a110180bebff5303
IrinaSelitskaya <119079534+IrinaSelitskaya@users.noreply.github.com>
2023-02-08 18:15:23 +0300

    Update AlphabeticCharacters.java

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,7 +2,6 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
-     character=65;
      if(character>=65 && character<=90 || character>=97 && character<=122){
       if(character==65 || character==69 || character==73 || character==79 || character==85 || character==97 || character==101 || character==105 || character==111 || character==117){
        System.out.println("Vowel");} 
